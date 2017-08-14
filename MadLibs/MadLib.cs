616d4beff3acc9c9655383627e6012ab3b99742b
Nathan Strisower <doogperson@hotmail.com>
2017-08-14 11:48:27 -0700

    Added MadLib.cs, started a sample Mad Lib Added the "MadLib.cs" class; it has string name and string[][] mdLibContainer, where string[0][] is the mad lib, string[1][] is the word type prompt, and string[2][] is the user-submitted word.

---
+++ MadLibs/MadLib.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadLibs
+{
+    public class MadLib
+    {
+        public string[][] madLib { get; set; }
+        public string title { get; set; }
+
+        public MadLib(string[][] mL, string t)
+        {
+            mL = madLib;
+            t = title;
+        }
+    }
+}
--- MadLibs/Program.cs
+++ MadLibs/Program.cs
@@ -10,8 +10,13 @@ namespace MadLibs
     {
         static void Main(string[] args)
         {
-            // ahoy matey
-            int hoi;
+            string[][] mLContainer = new string[3][];
+            mLContainer[0][0] = "Everyone was lined up at the ";
+            mLContainer[1][0] = "VERB ENDING IN 'ING'";
+            mLContainer[0][1] = " line. The faces of the ";
+            mLContainer[1][1] = "PLURAL NOUN";
+            mLContainer[0][2] = " were tense and expectant. Suddenly, the ";
+            MadLib mL = new MadLib(mLContainer, "The Race");
         }
     }
 }
